27be21c8ba5f82599955ec54751717dbbc70d811
Trần Hồng Thái <hongthaiovi@gmail.com>
2020-11-06 16:26:02 +0700

    Add local freshchat

--- android/src/main/java/com/freshchat/consumer/sdk/react/RNFreshchatSdk.java
+++ android/src/main/java/com/freshchat/consumer/sdk/react/RNFreshchatSdk.java
@@ -6,10 +6,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
